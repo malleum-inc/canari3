83913a2149c9610f3e4419c5badabf7453b79ae0
allfro <ndouba@redcanari.com>
2015-10-09 01:45:25 -0400

    Added detection of superuser transforms and functionality to elevate privileges prior to running the shell.

--- src/canari/commands/shell.py
+++ src/canari/commands/shell.py
@@ -5,7 +5,7 @@ from atexit import register
 
 from canari.mode import CanariMode, set_canari_mode
 from canari.pkgutils.transform import TransformDistribution
-from common import canari_main, fix_binpath, import_package
+from common import canari_main, fix_binpath, import_package, parse_bool
 from canari.utils.fs import pushd
 from framework import SubCommand, Argument
 from canari.config import config
@@ -24,6 +24,12 @@ __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
+def sudo():
+    print highlight("Need to be root to run this transform... sudo'ing...", 'green', True)
+    sys.argv.insert(0, 'sudo')
+    os.execvp(sys.argv[0], sys.argv)
+
+
 class ShellCommand(object):
 
     def __init__(self, transform):
@@ -31,9 +37,7 @@ class ShellCommand(object):
 
     def __call__(self, value, *args, **kwargs):
         if os.name == 'posix' and self.transform.superuser and os.geteuid():
-            print highlight("Need to be root to run this transform... sudo'ing...", 'green', True)
-            sys.argv.insert(0, 'sudo')
-            os.execvp(sys.argv)
+            sudo()
         if args and isinstance(args[0], dict):
             kwargs.update(args[0])
             args = args[1:]
@@ -42,15 +46,23 @@ class ShellCommand(object):
 
 class MtgConsole(InteractiveConsole):
 
-    def __init__(self, transform_classes):
+    def __init__(self, transform_classes, auto_sudo=False):
         locals_ = {}
+        asked = False
         for transform_class in transform_classes:
             transform = transform_class()
             locals_['do' + transform.name.split('.')[-1]] = ShellCommand(transform)
-        InteractiveConsole.__init__(self, locals=locals_)
-        self.init_history(os.path.expanduser('~/.mtgsh_history'))
+            if not asked and transform.superuser and os.name == 'posix' and os.geteuid():
+                if not auto_sudo and parse_bool("A transform requiring 'root' access was detected."
+                                                " Would you like to run this shell as 'root'?", False):
+                    sudo()
+                asked = True
 
-    def init_history(self, history_file):
+        InteractiveConsole.__init__(self, locals=locals_)
+        MtgConsole.init_history(os.path.expanduser('~/.mtgsh_history'))
+
+    @staticmethod
+    def init_history(history_file):
         try:
             import readline
             readline.parse_and_bind('tab: complete')
@@ -82,6 +94,13 @@ class MtgConsole(InteractiveConsole):
     help="the path that will be used as the working directory for "
          "the transforms being executed in the shell (default: ~/.canari/)"
 )
+@Argument(
+    '-s',
+    '--sudo',
+    action='store_true',
+    default=False,
+    help='Instructs the shell to automatically elevate privileges to root if necessary.'
+)
 def shell(opts):
 
     set_canari_mode(CanariMode.LocalShellDebug)
@@ -94,7 +113,7 @@ def shell(opts):
     try:
         transform_package = TransformDistribution(opts.package)
         with pushd(opts.working_dir or transform_package.default_prefix):
-            mtg_console = MtgConsole(transform_package.transforms)
+            mtg_console = MtgConsole(transform_package.transforms, auto_sudo=opts.sudo)
             mtg_console.interact(highlight('Welcome to Canari %s.' % canari.__version__, 'green', True))
     except ValueError, e:
         print str(e)
