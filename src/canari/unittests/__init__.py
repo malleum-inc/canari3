4b5c5893c149426e56b912fb0c607e254ef3ae94
allfro <ndouba@redcanari.com>
2016-01-21 21:30:08 -0500

    ENH: implemented "run all unit tests" code

--- src/canari/unittests/__init__.py
+++ src/canari/unittests/__init__.py
@@ -1,3 +1,5 @@
+import unittest
+
 __author__ = 'Nadeem Douba'
 __copyright__ = 'Copyright 2015, Canari Project'
 __credits__ = []
@@ -7,3 +9,7 @@ __version__ = '0.1'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
+
+if __name__ == "__main__":
+    all_tests = unittest.TestLoader().discover('canari.unittests', pattern='*.py')
+    unittest.TextTestRunner().run(all_tests)
