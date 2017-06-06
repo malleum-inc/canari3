668c27ec01b71e77078e0b585c7d62a302373444
allfro <ndouba@gmail.com>
2017-06-05 21:01:36 -0400

    Add if statement for pushing images

--- build_dockers.sh
+++ build_dockers.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+PREFERED_BUILD=alpine
+
 echo "Make sure your Docker environment has been set"
 
 SOURCE_DIR=`dirname ${BASH_SOURCE[0]}`
@@ -10,16 +12,25 @@ pushd src
 
 VERSION=`python -c 'import canari; print canari.__version__'`
 
+if [ -n "$CIRCLE_TAG" ]; then
+    VERSION="${CIRCLE_TAG#*v}"
+fi;
+
 popd
 
 echo $SOURCE_DIR
 
 pushd $SOURCE_DIR
 
-docker build -t redcanari/canari:$VERSION-alpine -f Dockerfile-alpine .
-docker build -t redcanari/canari:$VERSION-ubuntu -f Dockerfile-ubuntu .
-docker build -t redcanari/canari:$VERSION-kalilinux -f Dockerfile-kalilinux .
-docker tag -f redcanari/canari:$VERSION-alpine redcanari/canari
-#docker push redcanari/canari
+for i in Docker*; do
+    docker build --rm=false -t "redcanari/canari:$VERSION-${i#*-}" -f $i .
+done
+
+
+docker tag redcanari/canari:$VERSION-$PREFERED_BUILD redcanari/canari
+
+if [ "$1" = "push" ]; then
+    docker push redcanari/canari
+fi;
 
 popd
