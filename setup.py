fce15210b79b21fe1c35fc4595f08ce576817c45
allfro <ndouba@gmail.com>
2017-06-06 09:39:09 -0400

    Added trove classifiers and long_description to setup file.

--- setup.py
+++ setup.py
@@ -4,6 +4,72 @@ import os
 
 from setuptools import setup, find_packages
 
+long_description = """
+Canari Framework 3 
+==================
+
+|Build Status| |Doc Status|
+
+Welcome to the Canari 3 repository - the next generation Maltego rapid
+transform development framework which allows you to rapidly prototype,
+package, and distribute Maltego local and remote transforms. Please
+visit the `documentation <http://canari3.readthedocs.io/en/latest/>`__
+site for a quick how-to and more in-depth information on the framework
+itself.
+
+Sneak Peek
+----------
+
+The following is an example of how easy it is to write a quick Maltego
+transform in Canari 3:
+
+.. code:: python
+
+
+    from canari.maltego.entities import Phrase, Person
+
+    class HelloWorld(Transform):
+        \"\"\"This transform says hello to a person entity.\"\"\"
+
+        # The transform input entity type.
+        input_type = Person
+
+        def do_transform(self, request, response, config):
+            return response + Phrase("Hello " + request.entity.value)
+
+Canari Docker
+-------------
+
+You can now dockerize your remote transform packages using
+``canari dockerize-package``. This will create a Docker container that
+runs Canari Plume fully configured with your remote transforms. You can
+easily distribute this container to your Docker swarm. Check out the
+documentation on Docker `website <http://docker.com>`__ for more
+information on how containers work.
+
+Bug Reports & Questions
+-----------------------
+
+Please use the issues page to log any bugs or questions regarding the
+Canari Framework.
+
+Kudos
+-----
+
+Kudos to our user community for making this release happen. A special
+thanks to those of you who have supported the development of Canari 3 by
+donating money at our crowd-funding pages. If you like this project,
+please consider donating money to help accelerate development.
+
+.. |Build Status| image:: https://circleci.com/gh/redcanari/canari3.svg?style=shield&circle-token=da787a222c75b0a739152d0aa92a9465f702bae6
+.. |Doc Status| image:: https://readthedocs.org/projects/canari3/badge/?version=latest
+
+
+"""
+
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+
 scripts = [
     'src/scripts/canari',
     'src/scripts/dispatcher',
@@ -25,6 +91,8 @@ requires = [
     'argparse',
     'flask',
     'Twisted',
+    'pyopenssl',
+    'service_identity',
     'safedexml',
     'lxml'
 ]
@@ -37,7 +105,8 @@ setup(
     author='Nadeem Douba',
     version='3.1.1',
     author_email='ndouba@gmail.com',
-    description='Rapid transform development and transform execution framework for Maltego.',
+    description='Canari Framework - Maltego rapid transform development and execution framework.',
+    long_description=long_description,
     license='GPLv3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
@@ -45,5 +114,29 @@ setup(
     scripts=scripts,
     zip_safe=False,
     install_requires=requires,
-    dependency_links=[]
+    dependency_links=[],
+    url='https://github.com/redcanari/canari',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server'
+    ]
 )
