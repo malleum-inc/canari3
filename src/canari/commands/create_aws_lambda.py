618f0f324006ac90e2f9591a82fd8d5e2c664d29
ndouba <ndouba@redcanari.com>
2018-07-05 14:24:14 -0400

    Added AWS upload and project copy

--- src/canari/commands/create_aws_lambda.py
+++ src/canari/commands/create_aws_lambda.py
@@ -2,8 +2,13 @@ from __future__ import print_function
 
 import sys
 import os
+from mimetypes import guess_type
+import shutil
 
-from pkg_resources import get_distribution
+import boto3
+from hashlib import md5
+
+from six import b
 
 if sys.version_info[0] > 2:
     from queue import Queue
@@ -15,8 +20,10 @@ from mrbob.configurator import Configurator
 from canari.commands.common import canari_main
 from canari.commands.framework import SubCommand, Argument
 from canari.pkgutils.transform import TransformDistribution
+from canari.resource import image_resources
 from canari.project import CanariProject
 from canari.utils.fs import PushDir
+
 __author__ = 'Nadeem Douba'
 __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
@@ -28,17 +35,18 @@ __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
-BLACKLISTED_REQUIREMENTS = [
-    'mr.bob',
-    'argparse',
-    'flask',
-    'Twisted',
-    'pyopenssl',
-    'service_identity',
-    'pyasn1',
-    'boto3'
-]
-
+S3_CDN_BUCKET_POLICY = """{
+  "Version":"2012-10-17",
+  "Statement":[{
+	"Sid":"PublicReadGetObject",
+        "Effect":"Allow",
+	  "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::%s/*"
+      ]
+    }
+  ]
+}"""
 
 # Queue for blocking until we get a result from hook to setup(). Ugly but effective :P
 q = Queue()
@@ -54,22 +62,50 @@ def hook_setup():
     setuptools.setup = setup_hook
 
 
-def blacklisted(requirement):
-    for b in BLACKLISTED_REQUIREMENTS:
-        if b == requirement.name:
-            return True
-    return False
-
-
 def get_dependencies(project):
     with PushDir(project.root_dir):
         hook_setup()
         import setup
-    dependencies = set([str(r) for r in get_distribution('canari').requires() if not blacklisted(r)])
-    return dependencies.union(q.get())
+    return set(q.get())
 
 
-# Argument parser
+def upload_images(project, opts):
+    bucket = opts.bucket or md5(b('canari-%s-transform-images' % project.name)).hexdigest()
+    region = opts.region_name or boto3.session.Session().region_name
+    print('Uploading image resources to AWS S3 bucket %r' % bucket, file=sys.stderr)
+
+    s3 = boto3.client(
+        's3',
+        # Hard coded strings as credentials, not recommended.
+        aws_access_key_id=opts.aws_access_key_id,
+        aws_secret_access_key=opts.aws_secret_access_key,
+        region_name=region
+    )
+
+    if bucket not in [bucket['Name'].lower() for bucket in s3.list_buckets()['Buckets']]:
+        print('Creating S3 bucket %r because it does not exist' % bucket)
+        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': region})
+
+    with PushDir(project.src_dir):
+        for i in image_resources(project.name):
+            img_name = os.path.basename(i)
+            dst_path = 'static/%s' % md5(b(img_name)).hexdigest()
+            print('Uploading %r to %r...' % (i, dst_path))
+            s3.upload_file(i, bucket, dst_path, ExtraArgs={'ContentType': guess_type(i)[0]})
+
+    print('Creating static website configuration for bucket %r...' % bucket)
+    s3.put_bucket_website(Bucket=bucket, WebsiteConfiguration={'IndexDocument': {'Suffix': 'index.html'}})
+
+    print('Adjusting bucket policy for public GetObject access...')
+    s3.put_bucket_policy(Bucket=bucket, Policy=S3_CDN_BUCKET_POLICY % bucket)
+
+    base_url = 'http://%s.s3-website.%s.amazonaws.com/static/' % (bucket, region)
+    print('Static images are now hosted at %r!' % base_url)
+
+    return base_url
+
+
+
 @SubCommand(
     canari_main,
     help="Adds AWS Lambda capability.",
@@ -83,14 +119,30 @@ def get_dependencies(project):
 )
 @Argument(
     '-r',
-    '--region',
+    '--region-name',
+    default=None,
     metavar='<AWS region>',
     help="The region to store the S3 objects in"
+)
+@Argument(
+    '-k',
+    '--aws-access-key-id',
+    default=None,
+    metavar='<AWS access key ID>',
+    help="AWS access key ID"
+)
+@Argument(
+    '-s',
+    '--aws-secret-access-key',
+    metavar='<AWS secret access key>',
+    help="AWS secret access key"
 )
 def create_aws_lambda(opts):
     try:
         project = CanariProject()
         target = os.path.join(project.root_dir, 'aws')
+
+        cdn_base_url = upload_images(project, opts)
 
         with PushDir(project.src_dir):
             transform_package = TransformDistribution(project.name)
@@ -112,7 +164,16 @@ def create_aws_lambda(opts):
             print('Generating Chalice project for %r in %r...' % (project.name, target), file=sys.stderr)
             configurator.render()
 
-            transform_package.configure(os.path.join(target, 'chalicelib'), remote=True)
+            transform_package.configure(os.path.join(target, 'chalicelib'), remote=True, additional_options={
+                'cdn': cdn_base_url
+            })
+
+            if os.path.lexists('../aws/foo'):
+                shutil.rmtree('../aws/foo')
+
+            print("Copying %r project tree to the 'aws' directory." % project.name)
+            shutil.copytree('foo', '../aws/foo',
+                            ignore=shutil.ignore_patterns("__pycache__", "*.pyc", ".*", "*.gif", "*.jpg", "*.jpeg", "*.png"))
 
             print("To deploy type 'chalice deploy' in the %r directory" % target)
             print('done!', file=sys.stderr)
