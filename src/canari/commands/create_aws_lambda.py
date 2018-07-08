91b0910b9008b9c35a24d254f3704e3d1b966b73
ndouba <ndouba@redcanari.com>
2018-07-07 22:38:43 -0400

    Fixed a few bugs

--- src/canari/commands/create_aws_lambda.py
+++ src/canari/commands/create_aws_lambda.py
@@ -95,35 +95,41 @@ def download_progress(count, size, total_bytes):
 def upload_images(project, opts):
     bucket = opts.bucket or md5(b('canari-%s-transform-images' % project.name)).hexdigest()
     region = opts.region_name or boto3.session.Session().region_name
-    print('Uploading image resources to AWS S3 bucket %r' % bucket, file=sys.stderr)
-
-    s3 = boto3.client(
-        's3',
-        # Hard coded strings as credentials, not recommended.
-        aws_access_key_id=opts.aws_access_key_id,
-        aws_secret_access_key=opts.aws_secret_access_key,
-        region_name=region
-    )
-
-    if bucket not in [bucket['Name'].lower() for bucket in s3.list_buckets()['Buckets']]:
-        print('Creating S3 bucket %r because it does not exist' % bucket)
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': region})
+    base_url = 'about:blank'
 
     with PushDir(project.src_dir):
-        for i in image_resources(project.name):
+        imgs = image_resources(project.name)
+
+        if not imgs:
+            return base_url
+
+        print('Uploading image resources to AWS S3 bucket %r' % bucket, file=sys.stderr)
+        s3 = boto3.client(
+            's3',
+            # Hard coded strings as credentials, not recommended.
+            aws_access_key_id=opts.aws_access_key_id,
+            aws_secret_access_key=opts.aws_secret_access_key,
+            region_name=region
+        )
+
+        if bucket not in [bkt['Name'].lower() for bkt in s3.list_buckets()['Buckets']]:
+            print('Creating S3 bucket %r because it does not exist' % bucket)
+            s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': region})
+
+        for i in imgs:
             img_name = os.path.basename(i)
             dst_path = 'static/%s' % md5(b(img_name)).hexdigest()
             print('Uploading %r to %r...' % (i, dst_path))
             s3.upload_file(i, bucket, dst_path, ExtraArgs={'ContentType': guess_type(i)[0]})
 
-    print('Creating static website configuration for bucket %r...' % bucket)
-    s3.put_bucket_website(Bucket=bucket, WebsiteConfiguration={'IndexDocument': {'Suffix': 'index.html'}})
+        print('Creating static website configuration for bucket %r...' % bucket)
+        s3.put_bucket_website(Bucket=bucket, WebsiteConfiguration={'IndexDocument': {'Suffix': 'index.html'}})
 
-    print('Adjusting bucket policy for public GetObject access...')
-    s3.put_bucket_policy(Bucket=bucket, Policy=S3_CDN_BUCKET_POLICY % bucket)
+        print('Adjusting bucket policy for public GetObject access...')
+        s3.put_bucket_policy(Bucket=bucket, Policy=S3_CDN_BUCKET_POLICY % bucket)
 
-    base_url = 'http://%s.s3-website.%s.amazonaws.com/static/' % (bucket, region)
-    print('Static images are now hosted at %r!' % base_url)
+        base_url = 'http://%s.s3-website.%s.amazonaws.com/static/' % (bucket, region)
+        print('Static images are now hosted at %r!' % base_url)
 
     return base_url
 
