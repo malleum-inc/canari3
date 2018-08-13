11629c0b76de5d3b036335e200f94fea7d3fa7a2
ndouba <ndouba@redcanari.com>
2018-08-13 15:48:04 -0400

    Cleaned up bash scripts

--- build_dockers.sh
+++ build_dockers.sh
@@ -8,28 +8,24 @@ echo "Make sure your Docker environment has been set"
 
 SOURCE_DIR=`dirname ${BASH_SOURCE[0]}`
 
-pushd src
-
-VERSION=`python -c 'import canari; print(canari.__version__)'`
+VERSION=`python setup.py --version`
 
 if [ -n "${CIRCLE_TAG}" ]; then
     VERSION="${CIRCLE_TAG#*v}"
 fi;
-
-popd
 
 echo "${SOURCE_DIR}"
 
 pushd "${SOURCE_DIR}"
 
 for i in Docker*; do
-    docker build --rm=false -t "redcanari/canari:$VERSION-${i#*-}" -f ${i} . || exit -1
+    docker build --rm=false -t "redcanari/canari:${VERSION}-${i#*-}" -f ${i} . || exit -1
 done
 
 
 docker tag redcanari/canari:${VERSION}-${PREFERRED_BUILD} redcanari/canari
 
-if [ "$1" = "push" ]; then
+if [ "${1}" = "push" ]; then
     docker push redcanari/canari
 fi;
 
