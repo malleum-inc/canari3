114889e553b65447a8a58e88a6fb6e0616a823f9
ndouba <ndouba@redcanari.com>
2018-08-13 15:40:18 -0400

    Cleaned up bash scripts

--- build_dockers.sh
+++ build_dockers.sh
@@ -18,16 +18,16 @@ fi;
 
 popd
 
-echo $SOURCE_DIR
+echo "${SOURCE_DIR}"
 
-pushd $SOURCE_DIR
+pushd "${SOURCE_DIR}"
 
 for i in Docker*; do
-    docker build --rm=false -t "redcanari/canari:$VERSION-${i#*-}" -f $i . || exit -1
+    docker build --rm=false -t "redcanari/canari:$VERSION-${i#*-}" -f ${i} . || exit -1
 done
 
 
-docker tag redcanari/canari:$VERSION-$PREFERRED_BUILD redcanari/canari
+docker tag redcanari/canari:${VERSION}-${PREFERRED_BUILD} redcanari/canari
 
 if [ "$1" = "push" ]; then
     docker push redcanari/canari
--- lambda/build_wheels.sh
+++ lambda/build_wheels.sh
@@ -7,45 +7,45 @@ environments="pip-3.6:3 pip-2.7:2"
 
 function download_wheels() {
 
-    local pip=$1
-    local python_major_version=$2
-    local working_dir="v$2"
+    local pip=${1}
+    local python_major_version=${2}
+    local working_dir="v${2}"
 
-    echo "Downloading Py$python_major_version wheels to $working_dir using $pip"
+    echo "Downloading Py${python_major_version} wheels to ${working_dir} using ${pip}"
 
     for d in "${@:3}"; do
-        echo "Collecting wheel for $d";
-        if [[ $d == *canari* ]]; then
-            $pip wheel --no-cache-dir --no-deps $d -w $working_dir
+        echo "Collecting wheel for ${d}";
+        if [[ ${d} == *canari* ]]; then
+            ${pip} wheel --no-cache-dir --no-deps ${d} -w "${working_dir}"
         else
-            $pip wheel --no-cache-dir $d -w $working_dir
+            ${pip} wheel --no-cache-dir ${d} -w "${working_dir}"
         fi
     done;
 
-    pushd $working_dir
+    pushd ${working_dir}
 
     local canari_version=$(ls canari* | cut -d '-' -f 2)
 
     for d in `ls`; do
-        unzip $d
-        rm $d
+        unzip "${d}"
+        rm "${d}"
     done
 
-    local zip_file="canari3-$canari_version-aws-lambda-deps-py$python_major_version.zip"
-    zip -r ../$zip_file *
+    local zip_file="canari3-${canari_version}-aws-lambda-deps-py${python_major_version}.zip"
+    zip -r "../${zip_file}" *
     popd
 
-    rm -rf $working_dir
+    rm -rf "${working_dir}"
 
-    if [[ $GITHUB_API_TOKEN == "" ]]; then
+    if [[ ${GITHUB_API_TOKEN} == "" ]]; then
         read -sp "Please enter your GitHub API Token: " GITHUB_API_TOKEN
     fi
 
-    ./upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN owner=redcanari repo=canari3 tag=v$canari_version filename=$zip_file
+    ./upload-github-release-asset.sh github_api_token=${GITHUB_API_TOKEN} owner=redcanari repo=canari3 tag=v${canari_version} "filename=${zip_file}"
 }
 
-for environment in $environments; do
-    pip=$(echo $environment | cut -d ':' -f 1)
-    python_major_version=$(echo $environment | cut -d ':' -f 2)
-    download_wheels $pip $python_major_version "${@:1}"
+for environment in ${environments}; do
+    pip=$(echo ${environment} | cut -d ':' -f 1)
+    python_major_version=$(echo ${environment} | cut -d ':' -f 2)
+    download_wheels ${pip} ${python_major_version} "${@:1}"
 done
