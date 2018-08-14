3452278e7b0a8c6a4233cd69b2fb7e857de9d8a1
ndouba <ndouba@redcanari.com>
2018-08-14 13:38:24 -0400

    bumped up the version

--- src/canari/__init__.py
+++ src/canari/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = 'Copyright 2015, Canari Project'
 __credits__ = []
 
 __license__ = 'GPLv3'
-__version__ = '3.3.8'
+__version__ = '3.3.9'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@redcanari.com'
 __status__ = 'Development'
