573404afb71a413c0f5482e0f81fdaed766b3146
ndouba <ndouba@redcanari.com>
2018-08-14 13:31:28 -0400

    Added no example skeleton generation

--- src/canari/commands/create_package.py
+++ src/canari/commands/create_package.py
@@ -19,31 +19,31 @@ __email__ = 'ndouba@redcanari.com'
 __status__ = 'Development'
 
 
-def create_package(package_name):
+def create_package(package_name, author, email, description, create_example):
 
     capitalized_package_name = package_name.capitalize()
 
     variables = {
         'project.name': package_name,
+        'project.description': description,
+        'project.create_example': create_example,
         'entity.example_name': 'My%sEntity' % capitalized_package_name,
         'entity.base_name': '%sEntity' % capitalized_package_name,
         'created.year': datetime.now().year,
         'canari.version': canari.version,
-        'canari.major_version': StrictVersion(canari.version).version[0]
-    }
-
-    defaults = {
-        'project.create_example': True,
-        'author.name': getuser()
+        'canari.major_version': StrictVersion(canari.version).version[0],
+        'author.name': author,
+        'author.email': email
     }
 
     if not path.exists(package_name):
         click.echo('creating skeleton in %s' % package_name, err=True)
-        configurator = Configurator('canari.resources.templates:create_package',
-                                    package_name,
-                                    {'non_interactive': False, 'remember_answers': True},
-                                    variables=variables,
-                                    defaults=defaults)
+        configurator = Configurator(
+            'canari.resources.templates:create_package%s' % ('' if create_example else '_no_example'),
+            package_name,
+            {'non_interactive': True, 'remember_answers': True},
+            variables=variables
+        )
         configurator.ask_questions()
         configurator.render()
     else:
